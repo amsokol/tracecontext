239eff18b3987349e8f21cfb96f5fadf5eec180c
Aleksandr Sokolovskii <amsokol+github@gmail.com>
2024-09-12 18:10:21 +0200

    Refactor dependencies and add support for tracestate in traceparent
    Signed-off-by: Aleksandr Sokolovskii <amsokol+github@gmail.com>

--- go.mod
+++ go.mod
@@ -3,3 +3,13 @@ module github.com/amsokol/tracecontext
 go 1.23.1
 
 require github.com/google/uuid v1.6.0
+
+require (
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel v1.30.0 // indirect
+	go.opentelemetry.io/otel/metric v1.30.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.30.0 // indirect
+	go.opentelemetry.io/otel/trace v1.30.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- traceparent/traceparent.go
+++ traceparent/traceparent.go
@@ -1,108 +1,79 @@
 package traceparent
 
 import (
+	"encoding/hex"
 	"errors"
 	"fmt"
-	"regexp"
-	"strings"
 
-	"github.com/google/uuid"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const (
 	// TraceparentVersion is the version of the traceparent header.
-	TraceparentVersion = "00"
-	// TraceparentFlag indicates that the trace is sampled.
-	TraceparentFlag = "01" // sampled
-
-	// TraceparentInvalidParentID is the value used for an invalid parentID.
-	TraceparentInvalidParentID = "0000000000000000" // invalid parentID
+	traceparentVersion = "00"
 
 	// TraceparentHTTPHeaderTag is the HTTP header tag for traceparent.
 	TraceparentHTTPHeaderTag = "traceparent"
+
+	// TraceparentHTTPHeaderTag is the HTTP header tag for traceparent.
+	TracestateHTTPHeaderTag = "tracestate"
 
 	// traceparentParts is the number of parts in a traceparent header.
 	traceparentParts = 4
 )
 
 var (
-	// reSpanID is a regular expression to validate span IDs.
-	reSpanID = regexp.MustCompile(`^[0-9a-f]{16}$`)
-
 	// errTraceparentInvalidFormat is returned when the traceparent format is invalid.
 	errTraceparentInvalidFormat = errors.New("invalid traceparent format")
-	// errSpanIDInvalidFormat is returned when the span ID format is invalid.
-	errSpanIDInvalidFormat = errors.New("invalid spanID format")
+	// errTraceparentInvalidVersion is returned when the traceparent version is invalid.
+	errTraceparentInvalidVersion = errors.New("invalid traceparent version")
 )
 
-// Traceparent represents the traceparent value.
-type Traceparent struct {
-	version  string
-	traceID  string
-	parentID string
-	flags    string
+func MarshalSpanContext(sc trace.SpanContext) string {
+	return fmt.Sprintf("%s-%s-%s-%s",
+		traceparentVersion, sc.TraceID().String(), sc.SpanID().String(), sc.TraceFlags().String())
 }
 
-// Serialize converts the Traceparent struct to a string.
-func (tp *Traceparent) String() string {
-	return fmt.Sprintf("%s-%s-%s-%s", tp.version, tp.traceID, tp.parentID, tp.flags)
-}
+func UnmarshalSpanContext(traceparent, tracestate string) (trace.SpanContextConfig, error) {
+	var version, traceID, parentID, flags string
 
-// WithNewParentID returns a new Traceparent with the provided parentID.
-func (tp *Traceparent) WithNewParentID(parentID string) (Traceparent, error) {
-	if !reSpanID.MatchString(parentID) {
-		return Traceparent{}, fmt.Errorf("%w: %s", errSpanIDInvalidFormat, parentID)
+	if n, err := fmt.Sscanf(traceparent, "%2s-%32s-%16s-%2s", &version, &traceID, &parentID, &flags); err != nil {
+		return trace.SpanContextConfig{}, fmt.Errorf("failed to parse traceparent: %w", err)
+	} else if n != traceparentParts {
+		return trace.SpanContextConfig{}, fmt.Errorf("%w: %s", errTraceparentInvalidFormat, traceparent)
 	}
 
-	return Traceparent{
-		version:  tp.version,
-		traceID:  tp.traceID,
-		parentID: parentID,
-		flags:    tp.flags,
+	if version != traceparentVersion {
+		return trace.SpanContextConfig{}, fmt.Errorf("%w: %s", errTraceparentInvalidVersion, version)
+	}
+
+	var cfgTraceID, cfgSpanID, cgfTraceFlags []byte
+
+	var cfgTraceState trace.TraceState
+
+	var err error
+
+	if cfgTraceID, err = hex.DecodeString(traceID); err != nil {
+		return trace.SpanContextConfig{}, fmt.Errorf("failed to decode trace ID: %w", err)
+	}
+
+	if cfgSpanID, err = hex.DecodeString(parentID); err != nil {
+		return trace.SpanContextConfig{}, fmt.Errorf("failed to decode parent ID: %w", err)
+	}
+
+	if cgfTraceFlags, err = hex.DecodeString(flags); err != nil {
+		return trace.SpanContextConfig{}, fmt.Errorf("failed to decode flags: %w", err)
+	}
+
+	if cfgTraceState, err = trace.ParseTraceState(tracestate); err != nil {
+		return trace.SpanContextConfig{}, fmt.Errorf("failed to parse tracestate: %w", err)
+	}
+
+	return trace.SpanContextConfig{
+		TraceID:    trace.TraceID(cfgTraceID),
+		SpanID:     trace.SpanID(cfgSpanID),
+		TraceFlags: trace.TraceFlags(cgfTraceFlags[0]),
+		TraceState: cfgTraceState,
+		Remote:     true,
 	}, nil
-}
-
-// New creates a new Traceparent with a generated traceID and with the provided parentID.
-func New(parentID string) (Traceparent, error) {
-	if !reSpanID.MatchString(parentID) {
-		return Traceparent{}, fmt.Errorf("%w: %s", errSpanIDInvalidFormat, parentID)
-	}
-
-	traceID, err := newTraceID()
-	if err != nil {
-		return Traceparent{}, fmt.Errorf("failed to generate traceID: %w", err)
-	}
-
-	return Traceparent{
-		version:  TraceparentVersion,
-		traceID:  traceID,
-		parentID: parentID,
-		flags:    TraceparentFlag,
-	}, nil
-}
-
-// newTraceID generates a new trace ID using UUID v7.
-func newTraceID() (string, error) {
-	uuid, err := uuid.NewV7()
-	if err != nil {
-		return "", fmt.Errorf("failed to generate UUID: %w", err)
-	}
-
-	return strings.ReplaceAll(uuid.String(), "-", ""), nil
-}
-
-// FromString parses a traceparent string and returns a Traceparent struct.
-func FromString(str string) (Traceparent, error) {
-	var tpt Traceparent
-
-	n, err := fmt.Sscanf(str, "%2s-%32s-%16s-%2s", &tpt.version, &tpt.traceID, &tpt.parentID, &tpt.flags)
-	if err != nil {
-		return Traceparent{}, fmt.Errorf("failed to parse traceparent: %w", err)
-	}
-
-	if n != traceparentParts {
-		return Traceparent{}, fmt.Errorf("%w: %s", errTraceparentInvalidFormat, str)
-	}
-
-	return tpt, nil
 }
